a57112d39e8f940f54f6f75449d522421e5313f4
65050691-glitch <65050691@kmitl.ac.th>
2025-09-17 12:22:53 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+num = int(input("EnterNum least than 25:"))
+if num <=25:
+    for i in range(num,26):
+
+        print("Inside the loop, my variable is ",i)
+else:
+    print("Error")
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,3 @@
+num = int(input("EnterNum:"))
+for i in range(0,13):
+    print( num,"*",i,"=",num*i)
