0386b75db85e32072f202eb4a7c093591eb51ae4
65050691-glitch <65050691@kmitl.ac.th>
2025-09-17 21:16:54 +0700

    Add files via upload

---
+++ cell04/ex03/float.py
@@ -0,0 +1,6 @@
+numberInput = input("Give me a number: ")
+number = float(numberInput)
+if number == int(number):
+        print("This number is an integer.")
+else:
+        print("This number is a decimal.")
